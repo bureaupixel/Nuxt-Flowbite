3935a3bd3a4ab80857de841fbec602ce4c7b6f7a
Bureaupixel <bram.overstijns@gmail.com>
2024-07-09 12:37:15 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+      <TitleBlock />
+      <client-only placeholder="loading...">
+        <Drawer />
+        
+      </client-only>
+      <NuxtPage />
+      <Footer />
+    </div>
+</template>
